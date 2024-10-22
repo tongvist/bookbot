bc37a3cf8c07fd636803edb10cd24f1fe351d7df
tongvist <67380624+tongvist@users.noreply.github.com>
2024-10-22 20:06:40 +0300

    added counting of characters

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 def main():
     path = "books/frankenstein.txt"
-    word_count = get_word_count(path)
+    word_count, word_list = get_word_count(path)
     print("Words in document:", word_count)
+
+    character_counts = get_char_counts(word_list)
+    print("Character counts in document:", character_counts)
 
 def get_word_count(text):
     with open(text, "r") as f:
         text = f.read()
         words = text.split()
-        return len(words)
+        return len(words), words
+
+def get_char_counts(words):
+    characters = {}
+
+    for word in words:
+        for letter in word:
+            letter = letter.lower()
+            if letter in characters:
+                characters[letter] += 1
+            else:
+                characters[letter] = 1
+
+    return characters
+
 main()
